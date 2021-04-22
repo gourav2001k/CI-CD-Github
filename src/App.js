2a45c8b1bb6ccbc8396e532e6e284f52f0286d6b
gourav2001k <mailatgourav2001@gmail.com>
2021-04-23 00:34:44 +0530

    Changed HomePage

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edited <code>src/App.js</code> and save to no reload.
-        </p>
+        <p>This is a demonstration of CI/CD PipeLine using GitHub Actions</p>
         <a
           className="App-link"
           href="https://reactjs.org"
